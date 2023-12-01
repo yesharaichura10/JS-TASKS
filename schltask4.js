10517350a0f65c917bd4b024943b41ef926e8c3b
yesharaichura.dreamspot <yesharaichura.dreamspot@gmail.com>
2023-12-01 17:52:20 +0530

    new file

---
+++ schltask4.js
@@ -0,0 +1,87 @@
+let student1 = [];
+let student2 = [];
+let student3 = [];
+const table1 = () => {
+    document.getElementById("tbl1").innerHTML = student1.map((item, index) => {
+        return (
+            `<tr>
+        <div>${item.stname}</div>
+        <div>${item.rollnum}</div>
+        <div>${item.suname}</div>
+        <div>${item.Course}</div>
+        <div>${item.scname}</div>
+        </tr>`
+        )
+    })
+}
+table1();
+
+const table2 = () => {
+    document.getElementById("tbl2").innerHTML = student2.map((item, index) => {
+        return (
+            `<tr>
+        <div>${item.stname}</div>
+        <div>${item.rollnum}</div>
+        <div>${item.suname}</div>
+        <div>${item.Course}</div>
+        <div>${item.scname}</div>
+        </tr>`
+        )
+    })
+}
+table2();
+
+const table3 = () => {
+    document.getElementById("tbl3").innerHTML = student3.map((item, index) => {
+        return (
+            `<tr>
+            <div>${item.stname}</div>
+            <div>${item.rollnum}</div>
+            <div>${item.suname}</div>
+            <div>${item.Course}</div>
+            <div>${item.scname}</div>
+            </tr>`
+        )
+    })
+}
+table3();
+
+const myfunction = () => {
+    const stname = document.getElementById("stname").value;
+    console.log(stname);
+    const rollnum = document.getElementById("rollnum").value;
+    console.log(rollnum);
+    const suname = document.getElementById("suname").value;
+    console.log(suname);
+    const Course = document.getElementById("Course").value;
+    console.log(Course);
+    const scname = document.getElementById("scname").value;
+    console.log(scname);
+    const person = { stname: stname, rollnum: rollnum, suname: suname, Course: Course, scname: scname }
+    switch (scname) {
+        case 'School-1': {
+            student1.push(person);
+            console.log(student1);
+            table1();
+            break;
+        }
+
+        case 'School-2': {
+            student2.push(person);
+            console.log(student2);
+            table2();
+            break;
+        }
+        case 'School-3': {
+            student3.push(person);
+            console.log(student3);
+            table3();
+            break;
+        }
+
+    }
+}
+
+
+
+
