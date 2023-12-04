7607d5db8683e65fb30277d7430893579be49260
yesharaichura.dreamspot <yesharaichura.dreamspot@gmail.com>
2023-12-04 18:14:00 +0530

    new file

---
+++ time2.js
@@ -0,0 +1,17 @@
+let timee = [];
+
+const myfunction =()=>{
+    const time1 = document.getElementById("time1").value;
+    console.log(time1);
+
+    const time2 = document.getElementById("time2").value;
+    console.log(time2);
+    console.log(parseInt(time1)-parseInt(time2));
+
+    if(parseInt(time1) - parseInt(time2)>1){
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+}
